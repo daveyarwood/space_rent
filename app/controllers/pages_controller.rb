c676050960a52c8ee90b6677dd1c34468c7e613a
Dave Yarwood <dave.yarwood@gmail.com>
2014-10-16 19:51:28 -0400

    restrict admin panel to admins

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
 class PagesController < ApplicationController
+  before_action :authorize, only: [:admin]
+
   def home
     @people = Person.all
-    @messages = Message.order(created_at: :desc).limit(10).reverse
+    @messages = Message.order(created_at: :desc).limit(20).reverse
+  end
+
+  def admin
+
+  end
+
+  private
+
+  def authorize
+    unless current_user.admin?
+      redirect_to root_path, error: "You have to be logged in as an admin to do that."
+    end
   end
 end
