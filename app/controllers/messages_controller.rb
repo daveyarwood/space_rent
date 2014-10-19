0f839b3e285870f83512a275ed4c9415610fba4e
Dave Yarwood <dave.yarwood@gmail.com>
2014-10-19 00:24:09 -0400

    display all messages on message index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.order(created_at: :desc).limit(20).reverse
+    @messages = Message.all
     
     respond_to do |format|
       format.html
