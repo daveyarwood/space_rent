9c2a73b71005d5e5e65ec5644766ca12057e7d0e
Dave Yarwood <dave.yarwood@gmail.com>
2014-10-18 23:57:47 -0400

    increase message index to 20 messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.order(created_at: :desc).limit(10).reverse
+    @messages = Message.order(created_at: :desc).limit(20).reverse
     
     respond_to do |format|
       format.html
