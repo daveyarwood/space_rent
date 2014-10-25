13f981a4c8296cbf4c3aaf3f28d95b257c7dbb9d
Dave Yarwood <dave.yarwood@gmail.com>
2014-10-25 00:06:26 -0400

    enable editing bills (for admins)

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,9 +1,9 @@
 class BillsController < ApplicationController
-  before_action :fetch_bill, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, only: [:edit, :update]
 
   def index
     @bills = Bill.where("owed > 0")
-    
+
     respond_to do |format|
       format.html
       format.json { render json: @bills }
@@ -11,27 +11,45 @@ class BillsController < ApplicationController
   end
 
   def show
-    
-  end
-  
-  def new
-    
-  end
-  
-  def edit
-    
-  end
-  
-  def update
-    
-  end
-  
-  def destroy
-    
-  end
-  
-  private
-  def fetch_bill
     @bill = Bill.find(params[:id])
-  end    
+  end
+
+  def new
+    @bill = Bill.new
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @bill.update(bill_params)
+      flash[:notice] = "Changes saved."
+    else
+      flash[:error] = "Something doesn't look right..."
+    end
+    render "edit"
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def bill_params
+    params.require(:bill).permit(:owed, :late)
+  end
+
+  def authorize
+    @bill = Bill.find(params[:id])
+    case
+    when !signed_in?
+      redirect_to root_path,
+        flash: {error: "You have to be signed in to do that."}
+    when !current_user.admin?
+      redirect_to root_path,
+        flash: {error: "Hey, you're not allowed to do that!"}
+    end
+  end
 end
